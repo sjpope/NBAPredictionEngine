d6e90546e2f5fb756293c6eaaeb31c7b9d7f9d88
Samuel J Pope <sjpope03@gmail.com>
2025-02-10 17:54:53 -0600

    More bootstrap code for Match simluation, Data Model definitions

---
+++ models.h
@@ -0,0 +1,35 @@
+# ifndef models
+# define models
+
+#include <string>
+using namespace std;
+
+
+struct Game {
+
+    string location;
+    Team home;
+    Team away;
+
+
+};
+
+struct Team {
+
+    string name;
+    Player players[5];
+
+
+};
+
+
+struct Player {
+
+    string name;
+    string team;
+    int position; // 1 thru 5;
+
+};
+
+
+# endif
